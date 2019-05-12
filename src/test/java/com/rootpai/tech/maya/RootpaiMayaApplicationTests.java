e737a7a3083cdfa39e846e0883ef5d4a52eb0901
Ankit Agarwal <ankitagarwal@Ankits-MacBook-Air.local>
2019-05-11 21:19:08 -0700

    initial commits

---
+++ src/test/java/com/rootpai/tech/maya/RootpaiMayaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rootpai.tech.maya;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RootpaiMayaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
